a933b8e65287342914deef1192f66a4090d4db97
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-08-22 11:49:14 +0100

    Switched db back to atlas.

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
 	try {
-		// const connect = await mongoose.connect(process.env.MONGO_URL)
-		// console.log(`Connected to Atlas`)
+		const connect = await mongoose.connect(process.env.MONGO_URL)
+		console.log(`Connected to Atlas`)
 
-		const connect = await mongoose.connect("mongodb://localhost:27017/urlshortener")
-		console.log(`Connected to MongoLocal`)
+		// const connect = await mongoose.connect("mongodb://localhost:27017/urlshortener")
+		// console.log(`Connected to MongoLocal`)
 	} catch (error) {
 		console.log(error)
 	}
