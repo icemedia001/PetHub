e90d83e24bc6d656e49a0d98435fe2672fd3063e
Theophilus Adeleke <theophilusadesola002@gmail.com>
2023-08-22 11:18:23 +0100

    Full url must be provided before the short one will be generated to avoided data validation error

--- Routes/shortUrlRoute.js
+++ Routes/shortUrlRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { urlShortener } from "../controllers/urlController.js"
+import { customUrlShortener, urlShortener } from "../controllers/urlController.js"
 
 const router = express.Router()
 
 //the post route that handles the full url.
 router.post("/short-url", urlShortener)
+router.post("/custom-url", customUrlShortener)
 
 export default router
--- controllers/urlController.js
+++ controllers/urlController.js
@@ -7,21 +7,29 @@ import shortId from "shortid"
 // this is the function that does that shortening work
 export const urlShortener = async (req, res) => {
 	const { fullUrl } = req.body
-	const shortUrl = shortId.generate()
 	try {
-		const match = await ShortUrl.findOne({ shortUrl })
-		if (match == null) {
-			const response = await ShortUrl.create({ fullUrl, shortUrl })
-			res.status(201).send({
-				success: true,
-				message: "Shortened a url successfully.",
-				response
-			})
+		if (!fullUrl) {
+			throw Error("Full url must be provided to get the short form")
 		} else {
-			throw Error("Url already in use")
+			const shortUrl = shortId.generate()
+			const match = await ShortUrl.findOne({ shortUrl })
+			if (match == null) {
+				const response = await ShortUrl.create({ fullUrl, shortUrl })
+				res.status(201).send({
+					success: true,
+					message: "Shortened a url successfully.",
+					response
+				})
+			} else {
+				throw Error("Url already in use")
+			}
 		}
 	} catch (error) {
-		console.log(error)
-		res.status(500).json({ success: false, message: "Something went wrong" })
+		res.status(500).json({ success: false, message: error.message })
 	}
+}
+
+export const customUrlShortener = async (req, res) => {
+	const allUrl = await ShortUrl.find({})
+	console.log(allUrl)
 }
